720924d5a6c1a45710bd9b5fdf73f678ec591a69
dbKeys <dbkeys@bitmark.io>
2018-09-01 15:50:59 +0000

    More readable formatting

--- day.php
+++ day.php
@@ -1,11 +1,34 @@
 <table>
 <tr>
-<td><img src="bs_1_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="bs_2_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="bs_3_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="bs_4_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="bs_5_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="bs_0_day.png?nocache=<?php echo time(); ?>" />
+<td><img src="bs_0_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_1_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_2_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_3_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_4_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_5_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_6_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_7_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_day.png?nocache=<?php echo time(); ?>" />
 </tr>
 <tr>
-<td><img src="hr_1_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="hr_2_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="hr_3_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="hr_4_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="hr_5_day.png?nocache=<?php echo time(); ?>" /></td><td></td>
+<td><img src="hr_0_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_1_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_2_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_3_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_4_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_5_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_6_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_7_day.png?nocache=<?php echo time(); ?>" /></td>
+<td></td>
 </tr>
 <tr>
-<td><img src="df_1_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="df_2_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="df_3_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="df_4_day.png?nocache=<?php echo time(); ?>" /></td><td><img src="df_5_day.png?nocache=<?php echo time(); ?>" /></td><td></td>
+<td><img src="df_0_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_1_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_2_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_3_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_4_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_5_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_6_day.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_7_day.png?nocache=<?php echo time(); ?>" /></td><td></td>
 </tr>
 </table>
