67f9e028fa29e5cc8db104731060e5f487cb8f26
dbKeys <dbkeys@bitmark.io>
2018-09-24 11:45:29 +0000

    Orthogonal Naming - Week is right name (instead of "Overall")

---
+++ week.php
@@ -0,0 +1,33 @@
+<table>
+<tr>
+<td><img src="bs_0.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_1.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_2.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_3.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_4.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_5.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_6.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs_7.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="bs.png?nocache=<?php echo time(); ?>" />
+</tr>
+<tr>
+<td><img src="hr_0.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_1.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_2.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_3.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_4.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_5.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_6.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="hr_7.png?nocache=<?php echo time(); ?>" /></td><td></td>
+</tr>
+<tr>
+<td><img src="df_0.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_1.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_2.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_3.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_4.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_5.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_6.png?nocache=<?php echo time(); ?>" /></td>
+<td><img src="df_7.png?nocache=<?php echo time(); ?>" /></td><td></td>
+</tr>
+</table>
